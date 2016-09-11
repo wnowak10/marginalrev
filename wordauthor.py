560b8a3751caf536700e77e2a6e0379c6de186e0
wnowak <wnowak10@gmail.com>
2016-09-11 19:44:02 -0400

    added comments, bag of words, sklean...still problems with training tree model

--- wordauthor.py
+++ wordauthor.py
@@ -2,25 +2,25 @@
 # date  = aug / sept 2016
 # purpose of this file = scrape content of MR posts.
 
-# progress so far = find <p> tags that are not a part of block quotes. 
-##MR blog commonly uses block quotes. since i am analyzing MR blog, i dont want to 
-## parse and analyze text of block quotes (at least not for now)
 
-# to do: 1. determine how to best export post content for ML analysis
-## 2. think of other feature engineering would be best done in python?
-## 2a. potentially # of links in a post? cowen more often includes posts that are just links
-##  3. add this functionality to my otherfunctions module so that i can get much data from one post all 
-## exported to a csv
+# to do 
+# use bag of words 
+# split test and train
+# to do tree classification
+
 
 #import libraries
+import re
 from bs4 import BeautifulSoup
 import requests
 import urllib2
 import csv
 # import functions from my code
-from otherfunctions import getLink
-from otherfunctions import makesoup 
-import pandas as pd
+from otherfunctions import getLink # from my other file. 
+from otherfunctions import makesoup  # from my other file. 
+import pandas as pd #pandas for DataFrame organization
+from nltk.corpus import stopwords # for stopwords (commonly used words to ignore)
+import nltk # for stopwords
 
 # get content for just today. today is "soup"
 ## make use of makesoup and getLink functions written in marginalrev/otherfunctions.py
@@ -31,9 +31,6 @@ today= makesoup(getLink(2016,9,4))
 ## [2] index, for example, shows that this is the third post of the day
 postThree=today.find_all('div', {'class' : 'pf-content'})[2]
 
-
-# show all <p> tags for this post
-# print postThree.find_all('p')
 
 
 # this function looks through a given post's soup and finds block quoted material
@@ -101,28 +98,98 @@ for i in days: # loop through days
 	numberofLinks=[]
 	for j in range(len(day.find_all('div', {'class' : 'pf-content'}))):
 		post=day.find_all('div', {'class' : 'pf-content'})[j]
-		quotes=findquotes(post)
-		postcontent.append({
+		quotes=findquotes(post) #find quotes for post j on day i
+		postcontent.append({ # make list with these elements
 			'author':BeautifulSoup(str(day.find_all('span',class_='meta_author_name')[j]),"html5lib").a.contents,
+			# above code looks at a post and finds author
 			'text':getposttext(findauthoredwords(post,quotes)),
-			'number of links':findNumberofLinks(findauthoredwords(post,quotes)),
-			'date':str(years)+'month ='+ str(1)+ 'day='+str(i),
-			'post number': j})
-		oneDay=pd.DataFrame(postcontent)
-	df = pd.concat([df,oneDay],ignore_index=True) #ignore index so that we have non-repeating indices
+			'number of links':findNumberofLinks(findauthoredwords(post,quotes)), #find links in non-quoted material. perhaps another useful feature?
+			'date':str(years)+'month ='+ str(1)+ 'day='+str(i), # rough way to identify date
+			'post number': j}) #id the post number
+		oneDay=pd.DataFrame(postcontent) # make a pd dataframe out of this list
+	df = pd.concat([df,oneDay],ignore_index=True) #concat data frames, ignore index so that we have non-repeating indices
 
 
 
 print df.shape
-
 print df.columns.values
 
-print df['text'][0]
+# print df['text'][0]  #this is a string object
 
-df.to_csv('out.csv') # export for kicks
+# df.to_csv('out.csv') # export for kicks
+# nltk.download()
+
+# print 'a' in stopwords.words("english") 
+
+# rely on kaggle heavily here
+# https://www.kaggle.com/c/word2vec-nlp-tutorial/details/part-1-for-beginners-bag-of-words
+
+
+def review_to_words( raw_review ):
+    # Function to convert a text to a processed string of words
+    # The input is a single string (a raw movie review), and 
+    # the output is a single string (a preprocessed movie review)
+    #
+
+    #
+    # 2. Remove non-letters        
+    letters_only = re.sub("[^a-zA-Z]", " ", raw_review) 
+    #
+    # 3. Convert to lower case, split into individual words
+    words = letters_only.lower().split()                             
+    #
+    # 4. In Python, searching a set is much faster than searching
+    #   a list, so convert the stop words to a set
+    stops = set(stopwords.words("english"))                  
+    # 
+    # 5. Remove stop words
+    meaningful_words = [w for w in words if not w in stops]   
+    #
+    # 6. Join the words back into one string separated by space, 
+    # and return the result.
+    return( " ".join( meaningful_words ))  
 
 
 
+
+
+# print review_to_words(df['text'][0]) #this works to do what we wanted. clean text for first post shown here
+
+num_text= df["text"].size # number of rows
+clean_train_text = []
+
+for i in xrange( 0, num_text ):
+    # Call our function for each one, and add the result to the list of
+    # clean reviews
+    clean_train_text.append( review_to_words( df["text"][i] ) )
+    if( (i+1)%3 == 0 ):
+    	print "text %d of %d\n" % ( i+1, num_text )                                                                    
+
+# print clean_train_text
+
+# DO SKLEARN
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+vectorizer = CountVectorizer(analyzer = "word",   
+                             tokenizer = None,    
+                             preprocessor = None, 
+                             stop_words = None,   
+                             max_features = 500) 
+
+train_data_features = vectorizer.fit_transform(clean_train_text)
+
+train_data_features = train_data_features.toarray()
+
+print train_data_features.shape
+
+vocab = vectorizer.get_feature_names()
+print vocab
+
+from sklearn.ensemble import RandomForestClassifier
+forest = RandomForestClassifier(n_estimators = 10)
+
+print df['author'][0][0]
 
 
 
